825f5747c978f4fde7eb598f27fbcff5e15f0a1d
SATHIEN <samuel.thien@mercedes-benz.com>
2026-07-13 17:22:50 +0800

    chore: remove poc code

--- dashboard/api/src/journal/journal.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, Matches } from 'class-validator';
-
-export class GenerateJournalDto {
-  @IsString()
-  @Matches(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/, {
-    message: 'weekStart must be YYYY-MM-DD format',
-  })
-  weekStart: string;
-}
